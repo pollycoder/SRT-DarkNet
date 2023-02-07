9d56e9408950020da505286ce211f362b118d262
Polly Zhou <polly@Pollys-MacBook-Pro.local>
2023-02-07 19:10:58 +0800

    add code

--- multi-tab/pkl2csv.py
+++ multi-tab/pkl2csv.py
@@ -1,7 +1,7 @@
 import pickle as pkl
 import pandas as pd
 
-with open(r'./datasets/X_test_NoDef.pkl', "rb") as f:
+with open(r'./datasets/X_train_NoDef.pkl', "rb") as f:
 	object = pkl.load(f)
 df = pd.DataFrame(object)
 df.to_csv(r'./datasets/X_test_NoDef.csv')
