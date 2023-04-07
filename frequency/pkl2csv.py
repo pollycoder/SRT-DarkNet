5caebd12d3e2d1401a7be245f044ae66520ab79c
Polly Zhou <polly@Pollys-MacBook-Pro.local>
2023-04-08 00:39:37 +0800

    reconstruct

--- frequency/pkl2csv.py
+++
@@ -1,9 +0,0 @@
-import pickle as pkl
-import pandas as pd
-import numpy as np
-
-with open(r'../datasets/1180filter/test.pickle', "rb") as f:
-	object = pkl.load(f, encoding='latin1')
-df = pd.DataFrame(object)
-df.to_csv(r'../datasets/test.csv')
-
